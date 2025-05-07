324316e61aa267ace6a65186a3ddaf3af428bc11
Francisco Panis Kaseker <franciscopk.ilax@gmail.com>
2025-05-07 14:03:38 -0300

    add printMessage() and commandExist(), more verbosity

--- ef-portal-installer.sh
+++ ef-portal-installer.sh
@@ -25,6 +25,11 @@ EF_PORTAL_SILENT_SETUP="false"
 JAVA_FILE_URL="https://www.ni-sp.com/wp-content/uploads/2019/10/jdk-11.0.19_linux-x64_bin.tar.gz"
 JAVA_FILE_NAME=$(basename $JAVA_FILE_URL)
 
+commandExists()
+{
+    command -v "$1" &> /dev/null
+}
+
 printMessage()
 {
 	if ! $EF_PORTAL_SILENT_SETUP
@@ -94,11 +99,14 @@ EOF
     source ~/.bashrc
 
 	printMessage "Installing needed packages..."
-    if cat /etc/os-release | egrep -iq "(ubuntu|debian)"
-    then
+	if commandExists apt
+	then
         sudo apt update -y
         sudo apt install unzip tar -y
-    else
+	fi
+    
+	if commandExists yum
+	then
         sudo yum install emacs-nox unzip tar -y
     fi
 }
@@ -111,28 +119,35 @@ setupEfportal()
         rm -f $JAVA_FILE_NAME
     fi
     
+	printMessage "Downloading Java..."
     wget --quiet --no-check-certificate $JAVA_FILE_URL
-    [ $? -ne 0 ] && echo "Failed to download >>> ${JAVA_FILE_NAME} <<<. Exiting..." && exit 1
+    [ $? -ne 0 ] && printMessage "Failed to download >>> ${JAVA_FILE_NAME} <<<. Exiting..." && exit 1
 
+	printMessage "Extracting Java..."
     sudo tar zxf $JAVA_FILE_NAME -C /usr/local/
-    [ $? -ne 0 ] && echo "Failed to extract >>> ${JAVA_FILE_NAME} <<<. Exiting..." && exit 2
+    [ $? -ne 0 ] && printMessage "Failed to extract >>> ${JAVA_FILE_NAME} <<<. Exiting..." && exit 2
     rm -f $JAVA_FILE_NAME
 
-    sudo bash -c "useradd -m ${EF_PORTAL_EFADMIN_USER} && useradd -m efnobody && rm -rf /opt/nisp/enginframe"
+
+	printMessage "Creating users..."
+    sudo bash -c "useradd -m ${EF_PORTAL_EFADMIN_USER} && useradd -m efnobody"
     echo -e "${EF_PORTAL_EFADMIN_PASSWORD}\n${EF_PORTAL_EFADMIN_PASSWORD}" | sudo passwd ${EF_PORTAL_EFADMIN_USER}
 
     if [ ! -f ${EF_PORTAL_JAR_NAME} ]
     then
+		printMessage "Downloading EF Portal jar file..."
         wget --quiet --no-check-certificate $EF_PORTAL_JAR_URL
-        [ $? -ne 0 ] && echo "Failed to download >>> ${EF_PORTAL_JAR_NAME} <<<. Exiting..." && exit 3
+        [ $? -ne 0 ] && printMessage "Failed to download >>> ${EF_PORTAL_JAR_NAME} <<<. Exiting..." && exit 3
     fi
 
     if [ ! -f ${EF_PORTAL_CONFIG_NAME} ]
     then
+		printMessage "Downloading EF Portal config file..."
         wget --quiet --no-check-certificate $EF_PORTAL_CONFIG_URL
-        [ $? -ne 0 ] && echo "Failed to download >>> ${EF_PORTAL_CONFIG_NAME} <<<. Exiting..." && exit 4
+        [ $? -ne 0 ] && printMessage "Failed to download >>> ${EF_PORTAL_CONFIG_NAME} <<<. Exiting..." && exit 4
     fi
 
+	printMessage "Configuring EF Portal..."
     sed -i "s/kernel.tomcat.https.port.*=.*/kernel.tomcat.https.port = $EF_PORTAL_HTTPS_PORT/" ${EF_PORTAL_CONFIG_NAME}
 
     # if slurm and dcvsm will ne enabled
@@ -158,11 +173,13 @@ setupEfportal()
         sed -i 's/system-auth/common-auth/' ${EF_PORTAL_CONFIG_NAME}
     fi
 
+	printMessage "Installing EF Portal..."
     sudo bash -c "export JAVA_HOME=/usr/local/jdk-11.0.19 && export PATH=\$JAVA_HOME/bin:\$PATH && umask 022 && java -jar ${EF_PORTAL_JAR_NAME} --batch -f ${EF_PORTAL_CONFIG_NAME}"
 
     return_code=$?
-    [ $return_code -ne 0 ] && echo "Failed to setup EF Portal. The exit error was >>> $return_code <<<. Exiting..." && exit 6
+    [ $return_code -ne 0 ] && printMessage "Failed to setup EF Portal. The exit error was >>> $return_code <<<. Exiting..." && exit 6
     
+	printMessage "Enabling and starting enginframe systemd service..."
     sudo systemctl enable --now enginframe.service
 
     rm -f $EF_PORTAL_JAR_NAME $EF_PORTAL_CONFIG_NAME
@@ -170,13 +187,13 @@ setupEfportal()
 
 printPassword()
 {
-    echo "User: ${EF_PORTAL_EFADMIN_USER}"
-    echo "Password: ${EF_PORTAL_EFADMIN_PASSWORD}"
+	printMessage "User: ${EF_PORTAL_EFADMIN_USER}"
+	printMessage "Password: ${EF_PORTAL_EFADMIN_PASSWORD}"
 }
 
 finishMessage()
 {
-    echo "EF Portal url: https://your_ip:8443"
+	printMessage "EF Portal url: https://your_ip:8443"
 }
 
 # main
@@ -193,5 +210,5 @@ checkParameters $@
 main
 
 # unknown error
-echo "Unknown error. Exiting..."
+printMessage "Unknown error. Exitting..."
 exit 255
